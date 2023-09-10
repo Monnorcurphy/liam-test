bda05292468542d7cbb3a2b677f49af9ac642a0b
Monnorcurphy <ConnorLMurphy@gmail.com>
2023-09-10 12:00:06 -0700

    Do the largest/smallest problem

--- javascript-questions/easy-questions/is-prime.js
+++ javascript-questions/easy-questions/is-prime.js
@@ -1,1 +1,18 @@
-# Write a function that determines if a given number is prime or not.
+// Write a function that determines if a given number is prime or not.
+
+function isPrime(number) {
+    let counter = 2;
+    let prime = true;
+    while (counter < number / 2) {
+        if (number % counter === 0) {
+            prime = false;
+        }
+        counter += 1
+    }
+    return prime;
+}
+console.log(isPrime(1))
+console.log(isPrime(3))
+console.log(isPrime(7))
+console.log(isPrime(10))
+console.log(isPrime(13))
--- javascript-questions/easy-questions/largest-and-smallest-array.js
+++ javascript-questions/easy-questions/largest-and-smallest-array.js
@@ -1,1 +1,20 @@
-# Given an array of numbers, write a function to find the largest and smallest numbers in the array.
+// Given an array of numbers, write a function to find the largest and smallest numbers in the array.
+
+function largestAndSmallest(numArray) {
+    let largest, smallest;
+    numArray.forEach(element => {
+        if (!largest || element > largest) {
+            largest = element
+        }
+        if (!smallest || element < smallest) {
+            smallest = element
+        }
+    });
+
+    return `The smallest number is ${smallest}, and the largest number is ${largest}!`
+}
+
+console.log(largestAndSmallest([0, 15, 6, -100, 10000, 6, 18]))
+console.log(largestAndSmallest([0, 0, 0, 0, 0, 0, 0]))
+console.log(largestAndSmallest([1]))
+
